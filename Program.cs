f15102bd31f9f94c7cbcc0928670ee1eef66fea9
Jcyern <juancarlosyernoespinosa@gmail.com>
2024-12-04 18:58:17 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+public class Program
+{
+
+
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Creating Repository ");
+    }
+}
